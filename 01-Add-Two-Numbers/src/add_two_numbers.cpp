9bed92b25adb4a4e3efab74292233946b1a4912e
aevalenc <aevalenc@syr.edu>
2022-07-31 10:41:40 +0200

    First implementation of add two numbers

--- 01-Add-Two-Numbers/src/add_two_numbers.cpp
+++ 01-Add-Two-Numbers/src/add_two_numbers.cpp
@@ -6,4 +6,14 @@
 
 #include "01-Add-Two-Numbers/src/add_two_numbers.h"
 
-ListNode *Solution::addTwoNumbers(ListNode *l1, ListNode *l2) { return {}; }
+ListNode *Solution::addTwoNumbers(ListNode *l1, ListNode *l2)
+{
+  ListNode *new_node{};
+  const auto value = l1->val + l2->val;
+
+  new_node->val = value;
+  new_node->next = nullptr;
+  l2->next = new_node;
+
+  return new_node;
+}
