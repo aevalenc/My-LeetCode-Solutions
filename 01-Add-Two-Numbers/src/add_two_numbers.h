1ac626e7a6826b7514c29eb17b121de260c06bb5
aevalenc <aevalenc@syr.edu>
2022-10-09 11:51:18 +0200

    Add some spaces for git testing

--- 01-Add-Two-Numbers/src/add_two_numbers.h
+++ 01-Add-Two-Numbers/src/add_two_numbers.h
@@ -6,15 +6,16 @@
 
 struct ListNode
 {
-  int val;
-  ListNode *next;
-  ListNode() : val(0), next(nullptr) {}
-  ListNode(int x) : val(x), next(nullptr) {}
-  ListNode(int x, ListNode *next) : val(x), next(next) {}
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
 class Solution
 {
-public:
-  ListNode *addTwoNumbers(ListNode *l1, ListNode *l2);
+  public:
+    ListNode *addTwoNumbers(ListNode *l1, ListNode *l2);
 };
+
